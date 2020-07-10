456f2c028ce332b5946d630f21d64a53b8cab333
Vinson Kok <vinsonkok@yahoo.com>
2020-07-10 17:36:27 -0500

    Enemy AI

---
+++ GMTK/Assets/Scripts/Allies.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Allies : MonoBehaviour
+{
+    public float currentHP;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TakeDamage(float damage)
+    {
+        // hurt vfx
+        // hurt sfx
+
+        currentHP -= damage;
+
+        if (currentHP <= 0)
+            Destroy(gameObject);
+    }
+}
