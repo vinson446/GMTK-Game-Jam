adacc1571cb018a0829d933256ca47ab72d0da34
Abdullah <anoor1375@gmail.com>
2020-07-10 17:35:53 -0500

    Player Movement
    Camera and Basic movement

---
+++ GMTK/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+   
+
+    public CharacterController controller;
+    public Transform camera;
+    public float moveSpeed = 6f;
+
+
+    public float turnSpeed = 0.1f;
+
+    float turnVelocity;
+    void Start()
+    {
+        Cursor.visible = false;
+
+    }
+
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized; 
+
+        if(direction.magnitude>= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg+ camera.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnVelocity, turnSpeed);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDirection.normalized * moveSpeed * Time.deltaTime);
+        }
+
+    }
+}
