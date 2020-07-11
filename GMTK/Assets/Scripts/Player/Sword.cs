e02204b76124682b23a219d52114c9d4a5a107fa
Abdullah <anoor1375@gmail.com>
2020-07-10 20:11:44 -0500

    Weapon animation and script

---
+++ GMTK/Assets/Scripts/Player/Sword.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sword : MonoBehaviour
+{
+    [Header("Assign")]
+
+    public Animator animator;
+    public Transform attackPoint;
+    public LayerMask enemyLayer;
+    [Header("Attack")]
+    public float attackRange = 2f;
+    public float damage = 5f;
+
+    public int numberOfClicks = 0;
+    public float comboDelay =2f;
+     float lastClickTime =0f;
+   
+
+    void Start()
+    {
+
+        animator = GetComponent<Animator>();
+
+    }
+
+     void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Attack();
+        }
+    }
+
+   
+
+    void Attack()
+    {
+        animator.SetBool("Attack1", true);
+        HitDetection();
+
+    }
+
+    public void Swing1()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            animator.SetBool("Attack2", true);
+            HitDetection();
+
+        }
+        else
+        {
+            animator.SetBool("Attack1", false);
+        }
+    }
+
+    public void Swing2()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            animator.SetBool("Attack3", true);
+            HitDetection();
+        }
+        else
+        {
+            animator.SetBool("Attack2", false);
+        }
+    }
+    public void Swing3()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            HitDetection();
+
+        }
+        animator.SetBool("Attack1", false);
+        animator.SetBool("Attack2", false);
+        animator.SetBool("Attack3", false);
+    }
+
+
+
+    public void HitDetection()
+    {
+        Collider[] hitEneimes = Physics.OverlapSphere(attackPoint.position, attackRange, enemyLayer);
+
+        foreach (Collider enemy in hitEneimes)
+        {
+            enemy.GetComponent<Enemy>().TakeDamage(damage);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null)
+            return;
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
+}
